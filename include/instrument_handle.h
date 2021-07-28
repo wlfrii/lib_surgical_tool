dfc86fbc6da69549568e075b75c8767b0ae01442
Apple <apple@xiaoxueyuanya-2.local>
2021-07-28 11:33:03 +0800

    Compelte the CMakeLists

--- include/instrument_handle.h
+++ include/instrument_handle.h
@@ -1,11 +1,14 @@
 #ifndef INSTRUMENT_HANDLE_H
 #define INSTRUMENT_HANDLE_H
-
+#include <stdio.h>
 class InstrumentHandle
 {
 public:
     InstrumentHandle();
 
+    void libtest(){
+        printf("Call the lib_instrument successfully!\n");
+    }
 };
 
 #endif // INSTRUMENT_HANDLE_H
--- test/main.cpp
+++ test/main.cpp
@@ -2,6 +2,9 @@
 
 GTEST_API_ int main(int argc, char* argv[])
 {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    printf("Test successfully!\n");
+    InstrumentHandle handle;
+    handle.libtest();
+    //testing::InitGoogleTest(&argc, argv);
+    return 0;//RUN_ALL_TESTS();
 }
